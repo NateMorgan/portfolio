b51d8dc8cd485eaf49e82f2cb82956b417914348
Nate Morgan <nathanielrmorgan@gmail.com>
2022-10-15 17:50:54 -0400

    initial resume page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import LandingPage from './pages/LandingPage/LandingPage';
+import Resume from './pages/Resume/Resume';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <NavBar/>
-      <LandingPage/>
+      {/* <LandingPage/> */}
+      <Resume/>
       <Footer/>
     </div>
   );
--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,7 +1,14 @@
 const Resume = (props) => {
   return ( 
     <main>
-      
+      <embed 
+        type="application/pdf" 
+        src="/resume.pdf"
+        height="95%"
+        width="80%"
+        />
+      <br />
+      <button>Download Resume</button>
     </main>
    );
 }
