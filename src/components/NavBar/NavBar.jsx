039f5033a4a9fcb2d6453675b92d227c60af2469
Nate Morgan <nathanielrmorgan@gmail.com>
2022-10-22 14:33:51 -0400

    Nav bar links to NavLinks

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = (props) => {
   return ( 
     <nav>
-      <Link to='/about-me'> <h5> About Me</h5> </Link>
-      <Link to='/projects'> <h5> Projects</h5> </Link>
-      <Link to='/resume'> <h5> Resume</h5> </Link>
-      <Link to='/contact'> <h5> Contact Me</h5> </Link>
+      <NavLink to='/about-me'> <h5> About Me</h5> </NavLink>
+      <NavLink to='/projects'> <h5> Projects</h5> </NavLink>
+      <NavLink to='/resume'> <h5> Resume</h5> </NavLink>
+      <NavLink to='/contact'> <h5> Contact Me</h5> </NavLink>
     </nav>
    );
 }
