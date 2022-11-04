085594f53bde1e6d8a0736e669d1d16ea014deda
Nate Morgan <nathanielrmorgan@gmail.com>
2022-11-04 18:34:31 -0400

    reduce file size

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = (props) => {
         </div>
         <div className="logoPic">
           <a href="https://github.com/NateMorgan" target="_blank" rel="noreferrer">
-            <img className="contact-img" src="/img/logos/GitHub.png" alt="Github"/>
+            <img className="contact-img" src="/img/logos/github.svg" alt="Github"/>
           </a>
         </div>
         <div className="contactRef">
@@ -26,7 +26,7 @@ const Contact = (props) => {
         </div>
         <div className="logoPic">
           <a href="https://www.linkedin.com/in/nathaniel-r-morgan/" target="_blank" rel="noreferrer">
-            <img className="contact-img" src="/img/logos/LinkedIn.png" alt="LinkedIn"/>
+            <img className="contact-img" src="/img/logos/linkedin.svg" alt="LinkedIn"/>
           </a>
         </div>
         <div className="contactRef">
@@ -37,7 +37,7 @@ const Contact = (props) => {
         </div>
         <div className="logoPic">
           <a href="https://twitter.com/Nate_R_Morgan" target="_blank" rel="noreferrer">
-            <img className="contact-img" src="/img/logos/Twitter.png" alt="Twitter"/>
+            <img className="contact-img" src="/img/logos/twitter.svg" alt="Twitter"/>
           </a>
         </div>
         <div className="contactRef">
@@ -46,12 +46,6 @@ const Contact = (props) => {
               <a href="https://twitter.com/Nate_R_Morgan" target="_blank" rel="noreferrer">https://twitter.com/Nate_R_Morgan</a>
             </p>
         </div>
-        {/* <div className="logoPic">
-            <img className="contact-img" src="/img/logos/Youtube.png" alt="YouTube"/>
-        </div>
-        <div className="contactRef">
-            <p> <b> YouTube: </b> INSERT YOUTUBE URL HERE</p>
-        </div> */}
       </div>
       <div id="rightSide">
           <img className="headshot" src="img/headshot2.jpg" alt="Headshot of Nathaniel Morgan"/>
